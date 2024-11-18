d7713a22c5aa8562f6d2361f2e00ad322a7470c8
Legend-Chu <1614168079@qq.com>
2024-11-18 15:27:12 +0800

    添加客户端发送消息的功能

--- main.cpp
+++ main.cpp
@@ -4,71 +4,105 @@
 #pragma comment(lib, "ws2_32.lib") // 加载 Winsock 库
 
 using namespace std;
+int startup(unsigned short *port)
+{
+    // 1. 初始化 Winsock 库
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0)
+    {
+        cerr << "WSAStartup failed: " << result << endl;
+        return -1;
+    }
+
+    // 2. 创建套接字
+    int server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server_sock == INVALID_SOCKET)
+    {
+        cerr << "Socket creation failed: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return -1;
+    }
+
+    // 3. 配置服务器地址结构
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // 监听所有网络接口
+    server_addr.sin_port = htons(*port);             // 绑定到指定端口
+
+    // 4. 绑定套接字
+    result = bind(server_sock, (sockaddr *)&server_addr, sizeof(server_addr));
+    if (result == SOCKET_ERROR)
+    {
+        cerr << "Bind failed: " << WSAGetLastError() << endl;
+        closesocket(server_sock);
+        WSACleanup();
+        return -1;
+    }
+
+    // 5. 设置监听队列，等待客户端连接
+    result = listen(server_sock, SOMAXCONN);
+    if (result == SOCKET_ERROR)
+    {
+        cerr << "Listen failed: " << WSAGetLastError() << endl;
+        closesocket(server_sock);
+        WSACleanup();
+        return -1;
+    }
+
+    // 6. 动态分配端口，返回绑定的端口号
+    sockaddr_in addr;
+    int addr_len = sizeof(addr);
+    result = getsockname(server_sock, (sockaddr *)&addr, &addr_len);
+    if (result == SOCKET_ERROR)
+    {
+        cerr << "Getsockname failed: " << WSAGetLastError() << endl;
+        closesocket(server_sock);
+        WSACleanup();
+        return -1;
+    }
+    *port = ntohs(addr.sin_port); // 将端口号从网络字节序转回主机字节序
+
+    return server_sock;
+}
 
 int main()
 {
-    WSADATA wsaData;
-    SOCKET serverSocket, clientSocket;
-    sockaddr_in serverAddr, clientAddr;
-    int clientAddrLen = sizeof(clientAddr);
-
-    // 初始化 Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    while (true)
     {
-        cout << "Winsock initialization failed!" << endl;
-        return 1;
+        unsigned short port = 8080; // 你可以设置一个固定端口或者将端口号设为0，操作系统会动态分配
+        int server_sock = startup(&port);
+        if (server_sock == -1)
+        {
+            return -1; // 启动失败
+        }
+
+        cout << "HTTP服务器已启动，正在监听端口 " << port << "..." << endl;
+        // 7. 等待并接受客户端连接
+        sockaddr_in client_addr;
+        int client_addr_len = sizeof(client_addr);
+        SOCKET client_sock = accept(server_sock, (sockaddr *)&client_addr, &client_addr_len);
+        if (client_sock == INVALID_SOCKET)
+        {
+            cerr << "Accept failed: " << WSAGetLastError() << endl;
+            continue;
+        }
+
+        cout << "收到来自 " << inet_ntoa(client_addr.sin_addr) << " 的连接" << endl;
+
+        // 8. 处理客户端请求
+        const char *http_response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Connection: close\r\n\r\n"
+            "Hello, World!\n";
+
+        send(client_sock, http_response, strlen(http_response), 0);
+        closesocket(client_sock); // 关闭客户端连接
     }
 
-    // 创建套接字
-    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (serverSocket == INVALID_SOCKET)
-    {
-        cout << "Socket creation failed!" << endl;
-        WSACleanup();
-        return 1;
-    }
-
-    // 配置服务器地址
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(8080);
-
-    // 绑定套接字
-    if (bind(serverSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-    {
-        cout << "Bind failed!" << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // 开始监听
-    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
-    {
-        cout << "Listen failed!" << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    cout << "Server is listening on port 8080..." << endl;
-
-    // 接受客户端连接
-    clientSocket = accept(serverSocket, (sockaddr *)&clientAddr, &clientAddrLen);
-    if (clientSocket == INVALID_SOCKET)
-    {
-        cout << "Accept failed!" << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    cout << "Client connected!" << endl;
-
-    // 清理
-    closesocket(clientSocket);
-    closesocket(serverSocket);
-    WSACleanup();
-
+    // 9. 关闭服务器套接字
+    closesocket(server_sock);
+    WSACleanup(); // 清理 Winsock
     return 0;
 }
